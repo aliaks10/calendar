ffa5177cc11ecb069497459ae3d4e289619b878f
Aliaksei Barysau <abarysau.dev@gmail.com>
2022-04-14 15:16:50 +0300

    added antd css styles

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AppRouter from "./components/AppRouter";
 import {BrowserRouter} from "react-router-dom";
+import './App.css';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
